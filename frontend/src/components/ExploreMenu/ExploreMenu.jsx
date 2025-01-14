1b2a135c7a8298e1917018fa9f44aa65706ab7e5
Kushalshah0402 <kshah2407@gmail.com>
2025-01-14 16:24:05 +0800

    Changed the home page

--- frontend/src/components/ExploreMenu/ExploreMenu.jsx
+++ frontend/src/components/ExploreMenu/ExploreMenu.jsx
@@ -8,8 +8,8 @@ const ExploreMenu = ({category,setCategory}) => {
   
   return (
     <div className='explore-menu' id='explore-menu'>
-      <h1>Explore our menu</h1>
-      <p className='explore-menu-text'>Choose from a diverse menu featuring a delectable array of dishes. Our mission is to satisfy your cravings and elevate your dining experience, one delicious meal at a time.</p>
+      <h1>Explore our mart</h1>
+      <p className='explore-menu-text'>Choose from a wide selection of clothing, accessories, and essentials. We want to help you get what you need quickly and easily, so you can enjoy your items and feel your best every day.</p>
       <div className="explore-menu-list">
         {menu_list.map((item,index)=>{
             return (
